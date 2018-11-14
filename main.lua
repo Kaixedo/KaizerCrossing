7d5f76edc02095f1951e379c8f44ee451623764d
Kaixedo <chang_km@hotmail.com>
2018-11-14 20:11:28 +0000

    grid and more cars (#7)

--- main.lua
+++ main.lua
@@ -13,6 +13,24 @@ local function collision (ax, ay, aw, ah, bx, by, bw, bh)
   ax < bx + bw
 end
 
+--function to make player character move in grid instead of smoothly. Initial positions
+--and grid movement set to be in middle of road to increase difficulty. To do this, speed
+--set to 100 to increment to be in middle of road, and initial position to (400, 5).
+function love.keypressed(key)
+  if key == "up" then
+    Kaizer.y = Kaizer.y - Kaizer.speed
+  end
+  if key == "down" then
+    Kaizer.y = Kaizer.y + Kaizer.speed
+  end
+  if key == "left" then
+    Kaizer.x = Kaizer.x - Kaizer.speed
+  end
+  if key == "right" then
+    Kaizer.x = Kaizer.x + Kaizer.speed
+  end
+end
+
 function love.load()
   local imageKaizer = love.graphics.newImage("assets/Kaizer.jpg")
   local imageCar1 = love.graphics.newImage("assets/Car1.jpg")
@@ -21,40 +39,69 @@ function love.load()
 
   local car1 = {
     img = imageCar1,
-    x = 0, y = 100, w = imageCar1:getWidth(), h = imageCar1:getHeight(),
+    x = 0, y = 50, w = imageCar1:getWidth(), h = imageCar1:getHeight(),
     speed = 500, initialx = 0, endx = 1000
   }
   local car2 = {
       img = imageCar2,
-      x = 1000, y = 200, w = imageCar2:getWidth(), h = imageCar2:getHeight(),
+      x = 1000, y = 100, w = imageCar2:getWidth(), h = imageCar2:getHeight(),
       speed = -500, initialx = 1000, endx = 1000
   }
   local car3 = {
       img=imageCar3,
-      x=0, y=300, w=imageCar3:getWidth(), h=imageCar3:getHeight(),
+      x=0, y=150, w=imageCar3:getWidth(), h=imageCar3:getHeight(),
       speed = 300, initialx = 0, endx = 1000
   }
   local car4 = {
       img = imageCar1,
-      x = 2000, y = 400, w = imageCar1:getWidth(), h = imageCar1:getHeight(),
+      x = 2000, y = 200, w = imageCar1:getWidth(), h = imageCar1:getHeight(),
       speed = -700, initialx = 2000, endx = 2000
   }
   local car5 = {
       img=imageCar2,
-      x=0, y=500, w=imageCar2:getWidth(), h=imageCar2:getHeight(),
+      x=0, y=250, w=imageCar2:getWidth(), h=imageCar2:getHeight(),
       speed = -100, initialx = 1000, endx = 2000
   }
   local car6 = {
+      img=imageCar3, x=0, y=300, w=imageCar3:getWidth(), h=imageCar3:getHeight(),
+      speed = 400, initialx = 0, endx = 1000
+  }
+  local car7 = {
+    img = imageCar1,
+    x = 0, y = 350, w = imageCar1:getWidth(), h = imageCar1:getHeight(),
+    speed = 500, initialx = 0, endx = 1000
+  }
+  local car8 = {
+      img = imageCar2,
+      x = 1000, y = 400, w = imageCar2:getWidth(), h = imageCar2:getHeight(),
+      speed = -500, initialx = 1000, endx = 1000
+  }
+  local car9 = {
+      img=imageCar3,
+      x=0, y=450, w=imageCar3:getWidth(), h=imageCar3:getHeight(),
+      speed = 300, initialx = 0, endx = 1000
+  }
+  local car10 = {
+      img = imageCar1,
+      x = 2000, y = 500, w = imageCar1:getWidth(), h = imageCar1:getHeight(),
+      speed = -700, initialx = 2000, endx = 2000
+  }
+  local car11 = {
+      img=imageCar2,
+      x=0, y=550, w=imageCar2:getWidth(), h=imageCar2:getHeight(),
+      speed = -100, initialx = 1000, endx = 2000
+  }
+  local car12 = {
       img=imageCar3, x=0, y=600, w=imageCar3:getWidth(), h=imageCar3:getHeight(),
       speed = 400, initialx = 0, endx = 1000
   }
 
   Kaizer = {
       img=imageKaizer,
-      x=200, y=0, w=imageKaizer:getWidth(), h=imageKaizer:getHeight(),
-      speed = 500
+      x=400, y=5, w=imageKaizer:getWidth(), h=imageKaizer:getHeight(),
+      speed = 50
   }
-  EnemyCars = {car1, car2, car3, car4, car5, car6}
+  EnemyCars = {car1, car2, car3, car4, car5, car6, car7, car8, car9, car10, car11, car12}
 end
 
 function love.update(dt)
@@ -65,20 +112,6 @@ function love.update(dt)
       --table.remove(EnemyCars, i)
     end
   end
-
-  if love.keyboard.isDown("up") then
-    Kaizer.y = Kaizer.y - Kaizer.speed * dt -- this would increment num by 100 per second
-  end
-  if love.keyboard.isDown("down") then
-    Kaizer.y = Kaizer.y + Kaizer.speed * dt -- this would increment num by 100 per second
-  end
-  if love.keyboard.isDown("left") then
-    Kaizer.x = Kaizer.x - Kaizer.speed * dt -- this would increment num by 100 per second
-  end
-  if love.keyboard.isDown("right") then
-    Kaizer.x = Kaizer.x + Kaizer.speed * dt -- this would increment num by 100 per second
-  end
-
 end
 
 function love.draw()
