99b493eb072a7cefeb94f504bd19361557c8fb61
KM <chang_km@hotmail.com>
2018-11-20 22:11:14 +0000

    traps and deathcounter

--- main.lua
+++ main.lua
@@ -1,5 +1,7 @@
 local Kaizer = {}
 local EnemyCars = {}
+local EnemyTraps = {}
+local deathcounter=0
 
 --function to detect collision between Kaizer (player) and Cars. Inputs are
 --ax = topleft x coordinate of first object A (car), ay = topleft y coordinate of object A,
@@ -36,6 +38,8 @@ function love.load()
   local imageCar1 = love.graphics.newImage("assets/Car1.jpg")
   local imageCar2 = love.graphics.newImage("assets/Car2.jpg")
   local imageCar3 = love.graphics.newImage("assets/Car3.jpg")
+  local imagetrap1 = love.graphics.newImage("assets/Trap1.jpg")
+  local imagetrap2 = love.graphics.newImage("assets/Trap2.jpg")
 
   local car1 = {
     img = imageCar1,
@@ -96,12 +100,29 @@ function love.load()
       speed = 400, initialx = 0, endx = 1000
   }
 
+  local trap1 = {
+    img=imagetrap2, x=250, y=200, w=imagetrap2:getWidth(), h=imagetrap2:getHeight()
+  }
+  local trap2 = {
+    img=imagetrap2, x=250, y=300, w=imagetrap2:getWidth(), h=imagetrap2:getHeight()
+  }
+  local trap3 = {
+    img=imagetrap1, x=100, y=500, w=imagetrap1:getWidth(), h=imagetrap1:getHeight()
+  }
+  local trap4 = {
+    img=imagetrap1, x=400, y=500, w=imagetrap1:getWidth(), h=imagetrap1:getHeight()
+  }
+  local trap5 = {
+    img=imagetrap2, x=100, y=550, w=imagetrap2:getWidth(), h=imagetrap2:getHeight()
+  }
+
   Kaizer = {
       img=imageKaizer,
       x=400, y=5, w=imageKaizer:getWidth(), h=imageKaizer:getHeight(),
       speed = 50
   }
   EnemyCars = {car1, car2, car3, car4, car5, car6, car7, car8, car9, car10, car11, car12}
+  EnemyTraps = {trap1, trap2, trap3, trap4, trap5}
 end
 
 function love.update(dt)
@@ -117,6 +138,8 @@ end
 function love.draw()
   love.graphics.print(tonumber(Kaizer.x), 0, 10)
   love.graphics.print(tonumber(Kaizer.y), 0, 20)
+  love.graphics.print(tonumber(deathcounter),100,70)
+  love.graphics.print("deathcounter=",0,70)
 
   love.graphics.draw(Kaizer.img, Kaizer.x, Kaizer.y)
   for _, car in pairs(EnemyCars) do
@@ -126,6 +149,20 @@ function love.draw()
   for _, car in pairs(EnemyCars) do
     if collision(car.x, car.y, car.w, car.h, Kaizer.x, Kaizer.y, Kaizer.w, Kaizer.h) then
       love.graphics.print("COLLISION DETECTED", 0, 50)
+      deathcounter=deathcounter+1
     end
   end
+
+  --code for spike traps or water that instantly kills player
+  for _, trap in pairs(EnemyTraps) do
+    love.graphics.draw(trap.img, trap.x, trap.y)
+  end
+
+  for _, trap in pairs(EnemyTraps) do
+    if collision(trap.x, trap.y, trap.w, trap.h, Kaizer.x, Kaizer.y, Kaizer.w, Kaizer.h) then
+      love.graphics.print("IT'S A TRAP. YOU ARE DEAD.", 0, 60)
+      deathcounter=deathcounter+1
+    end
+  end
+
 end
